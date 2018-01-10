72d6a17964d1f8c9332c380f2267111a09788450
Thiago Vinhote <thiagovf26@outlook.com>
2018-01-10 09:19:59 -0400

    Novos campos
    Signed-off-by: Thiago Vinhote <thiagovf26@outlook.com>

--- src/app/models/party.js
+++ src/app/models/party.js
@@ -1,10 +1,26 @@
 const mongoose = require('../../database');
-const bcrypt = require('bcryptjs');
 
 const PartySchema = new mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  place: {
+    type: String,
+    required: true,
+  },
+  dateBegin: {
+    type: Date,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+    default: 'https://firebasestorage.googleapis.com/v0/b/partify-9c7f4.appspot.com/o/grupo_images%2F82068708-92C3-408D-A770-FB2C477CA90D?alt=media&token=3596551c-fb85-4ba5-928a-85a4171450f5'
   },
   createdAt: {
     type: Date,
